b6c0f3e3ddd3b70d436b859ed76c9bc7b5f0b4d4
Boutros99 <boutrosestephan@outlook.com>
2024-08-16 14:18:33 +0200

    created config file managing the postgre config depending on the environment

---
+++ db/config.js
@@ -0,0 +1,35 @@
+require('dotenv').config();
+const fs = require('fs');
+const path = require('path');
+
+
+const config = {
+    host: process.env.PGDHOST || process.env.DB_HOST, 
+    user: process.env.PGUSER || process.env.DB_USER,
+    database: process.env.PGDATABASE || process.env.DATABASE,
+    password: process.env.PGPASSWORD || process.env.DB_PASSWORD,
+    port: process.env.PGPORT || process.env.DB_PORT , // The default port
+  };
+
+
+if (process.env.NODE_ENV === 'production') {
+
+    // Load SSL/TLS root certificate from the environment variable
+    const sslRootCertPath = process.env.PGSSLROOTCERT;
+
+    // Read the root certificate file
+    let sslRootCert;
+    if (sslRootCertPath) {
+    sslRootCert = fs.readFileSync(path.resolve(sslRootCertPath)).toString();
+    } else {
+    throw new Error('PGSSLROOTCERT environment variable is not set.');
+    }
+
+    config.ssl = {
+        rejectUnauthorized: true,    // Validate server certificate
+        ca: sslRootCert,             // Root certificate
+      };
+    config.sslMode = process.env.PGSSLMODE || 'require'; // Optional: default to 'require'
+};
+
+module.exports = config;
